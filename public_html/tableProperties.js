7af4a60ad0779f41f0be41e890758cfe98da260b
matthew-was <matthew@wasbrough.co.uk>
2013-11-07 22:30:24 -0500

    tweeks

--- public_html/tableProperties.js
+++ public_html/tableProperties.js
@@ -8,20 +8,21 @@ $(function() {
     setTable = function(object) {
         var start = "<tr>";
         var end = "</tr>";
-        var name = "<td><input type='text' id='name' value="+object.name+" /></td>";
-        var wl = "<td><input type='text'value=" + object.wavelength + " /></td>";
-        var wlS = "<td><input type='text' value=" + object.wavelengthSpread + " /></td>";
-        var att = "<td><input type='text' value=" + object.attenuator + " /></td>";
-        var guide = "<td><input type='text' value=" + object.guides + " /></td>";
-        var guideAp = "<td><input type='text' value=" + object.guideAperture + " /></td>";
-        var bStop = "<td><input type='text' value=" + object.beamStop + " /></td>";
-        var bStopX = "<td><input type='text' value=" + object.beamStopX + " /></td>";
-        var bStopY = "<td><input type='text' value=" + object.beamStopY + " /></td>";
-        var bCentX = "<td><input type='text' value=" + object.beamCenterX + " /></td>";
-        var bCentY = "<td><input type='text' value=" + object.beamCenterY + " /></td>";
-        var detPos = "<td><input type='text' value=" + object.detectorPosition + " /></td>";
-        var detOff = "<td><input type='text' value=" + object.detectorOffset + " /></td></tr>";
-        return start+name+wl+wlS+att+guide+guideAp+bStop+bStopX+bStopY+bCentX+bCentY+detPos+detOff+end;
+        var name = "<td><input type='text' class='input' value="+object.name+" /></td>";
+        var wl = "<td><input type='text' class='input' value=" + object.wavelength + " /></td>";
+        var wlS = "<td><input type='text' class='input' value=" + object.wavelengthSpread + " /></td>";
+        var att = "<td><input type='text' class='input' value=" + object.attenuator + " /></td>";
+        var guide = "<td><input type='text' class='input' value=" + object.guides + " /></td>";
+        var guideAp = "<td><input type='text' class='input' value=" + object.guideAperture + " /></td>";
+        var sampAp = "<td><input type='text' class='input' value=" + object.sampleAperture + " /></td>";
+        var bStop = "<td><input type='text' class='input' value=" + object.beamStop + " /></td>";
+        var bStopX = "<td><input type='text' class='input' value=" + object.beamStopX + " /></td>";
+        var bStopY = "<td><input type='text' class='input' value=" + object.beamStopY + " /></td>";
+        var bCentX = "<td><input type='text' class='input' value=" + object.beamCenterX + " /></td>";
+        var bCentY = "<td><input type='text' class='input' value=" + object.beamCenterY + " /></td>";
+        var detPos = "<td><input type='text' class='input' value=" + object.detectorPosition + " /></td>";
+        var detOff = "<td><input type='text' class='input' value=" + object.detectorOffset + " /></td></tr>";
+        return start+name+wl+wlS+att+guide+guideAp+sampAp+bStop+bStopX+bStopY+bCentX+bCentY+detPos+detOff+end;
     };
     
 });
