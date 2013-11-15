4923530dd3cd3e893550d3cf189197b655a9c510
matthew.was <matthew@wasbrough.co.uk>
2013-11-15 13:04:01 -0500

    Added time and descriptions to sample panel

--- public_html/samples.js
+++ public_html/samples.js
@@ -14,27 +14,61 @@ $( function() {
             numSamps = 7;            
         };
         
-        var table = document.getElementById('sampTable');
-        var rowNum = table.rows.length;
-        if (rowNum !== 0){
-            for( var i = 0; i<rowNum; i++) {
-                table.deleteRow();
+        var sNTable = document.getElementById('sampNames');
+        var nRowNum = sNTable.rows.length;
+        if (nRowNum !== 0){
+            for( var i = 0; i<nRowNum; i++) {
+                sNTable.deleteRow();
             }
         }
-        
+
+        var sDTable = document.getElementById('sampDetail');
+        var dRowNum = sDTable.rows.length;
+        if (dRowNum !== 0){
+            for( var i = 0; i<dRowNum; i++) {
+                sDTable.deleteRow();
+            }
+        }
+
+        var sTTable = document.getElementById('sampTimes');
+        var tRowNum = sTTable.rows.length;
+        if (tRowNum !== 0){
+            for( var i = 0; i<tRowNum; i++) {
+                sTTable.deleteRow();
+            }
+        }
+
         //name, description, thickness, mass
         // don't forget sample aperture.
         
-        var tabRow0 = "<tr><th colspan=5>Sample</th></tr>";
-        var tabRow1 = "<tr><td class='col1' style='width:200px'></td><td class='col2'>Name</td><td class='col3'>Thickness</td><td class='col4'>Description</td><td class='col5'>Mass</td></tr>";
-        table.insertRow(0).innerHTML = tabRow0;
-        table.insertRow(1).innerHTML = tabRow1;
+        var sNRow0 = "<tr><th colspan=2>Sample</th></tr>";
+        var sNRow1 = "<tr><td id='row1' class='col1'></td><td class='col2'>Name</td></tr>";
+        var sNRow2 = "<tr></tr>";
+        var sDRow0 = "<tr><th colspan=3>Details</th></tr>";
+        var sDRow1 = "<tr><td id='row1' class='col3'>Thickness (mm)</td><td class='col4'>Description</td><td class='col5'>Mass (g)</td></tr>";
+        var sDRow2 = "<tr></tr>";
+        var sTRow0 = "<tr><th colspan=6>Times</th></tr>";
+        var sTRow1 = "<tr><td id='row2' colspan=2>1.1m5</td><td colspan=2>5m5</td><td colspan=2>5m14</td></tr>";
+        var sTRow2 = "<tr><td id='row2'>Scatt</td><td>Trans</td><td id='row2'>Scatt</td><td>Trans</td><td id='row2'>Scatt</td><td>Trans</td></tr>";
+        sNTable.insertRow(0).innerHTML = sNRow0;
+        sNTable.insertRow(1).innerHTML = sNRow1;
+        sNTable.insertRow(2).innerHTML = sNRow2;
+        sDTable.insertRow(0).innerHTML = sDRow0;
+        sDTable.insertRow(1).innerHTML = sDRow1;
+        sDTable.insertRow(2).innerHTML = sDRow2;
+        sTTable.insertRow(0).innerHTML = sTRow0;
+        sTTable.insertRow(1).innerHTML = sTRow1;
+        sTTable.insertRow(2).innerHTML = sTRow2;
         
         for (var i=0; i<numSamps; i++) {
             var j = i+1;
             
-            var tabRowN = "<tr><td class='col1'>"+j+"</td><td class='col2'><input type=text id=ic2 /></td><td class='col3'><input type=text id=ic3/></td><td class='col4'><input type=text class=ic4/></td><td class='col5'><input type=text clasee=ic5/></td></tr>";
-            table.insertRow(j+1).innerHTML = tabRowN;
+            var sNRowN = "<tr><td class='sCol1'>"+j+"</td><td class='sCol2'><input type=text class='sic2' /></td></tr>";
+            var sDRowN = "<tr><td class='dCol3'><input type=text class='dic3' value=1 /></td><td class='dCol4'><input type=text class='dic4'/></td><td class='dCol5'><input type=text class='dic5'/></td></tr>";
+            var sTRowN = "<tr><td><input type=text class='tbox' value=0 /></td><td><input type=text class='tbox' value=0 /></td><td><input type=text class='tbox' value=0 /></td><td><input type=text class='tbox' value=0 /></td><td><input type=text class='tbox' value=0 /></td><td><input type=text class='tbox' value=0 /></td></tr>";
+            sNTable.insertRow(j+2).innerHTML = sNRowN;
+            sDTable.insertRow(j+2).innerHTML = sDRowN;
+            sTTable.insertRow(j+2).innerHTML = sTRowN;
         }
     };
     
